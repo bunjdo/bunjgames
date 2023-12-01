a12bf4783da9c323e37d0402817d34679eb9309f
Artem Samokhin <bunjdo@gmail.com>
2023-12-01 22:39:32 +1300

    conf typo

--- conf/init.sh
+++ conf/init.sh
@@ -4,5 +4,5 @@ set -e
 
 docker swarm init
 docker network create --driver overlay caddy
-docker stack deploy --compose-file caddy/stack.yml caddy
-docker stack deploy --compose-file stack.yml bunjgames
+docker stack deploy --compose-file ./caddy/stack.yaml caddy
+docker stack deploy --compose-file stack.yaml bunjgames
